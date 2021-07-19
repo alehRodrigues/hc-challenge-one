4885f92e8487090e60e820ffb1c56147f6fd1b11
stenioas <stenioas@gmail.com>
2021-07-18 22:04:49 -0300

    refactor: card style changed

--- src/components/Card/styled.js
+++ src/components/Card/styled.js
@@ -11,6 +11,12 @@ export const CardBox = styled.div`
 		max-width: 15rem;
 		border-radius: 4px;
 		border: 2px solid #f7df1e;
+		transition: all 0.25s;
+
+		&:hover {
+			transform: scale(1.25);
+			cursor: pointer;
+		}
 	}
 
 	p {
